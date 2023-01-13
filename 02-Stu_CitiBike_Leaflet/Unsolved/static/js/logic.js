3cb9aafcb2b1417968edec644906b9e1d4954b9f
Kevin Ybarra <kevin.ybarra@gmail.com>
2023-01-12 19:35:59 -0600

    m

--- 02-Stu_CitiBike_Leaflet/Unsolved/static/js/logic.js
+++ 02-Stu_CitiBike_Leaflet/Unsolved/static/js/logic.js
@@ -1,23 +1,44 @@
-var newYorkCoords = [40.73, -74.0059];
-var mapZoomLevel = 12;
+// var newYorkCoords = [40.73, -74.0059];
+// var mapZoomLevel = 12;
 
 // Create the createMap function.
-
+let myMap = L.map("map", {
+  center: [40.73, -74.0059],
+  zoom: 12
+});
 
   // Create the tile layer that will be the background of our map.
-d3.json('https://gbfs.citibikenyc.com/gbfs/en/station_information.json')
-d3.json('(https://gbfs.citibikenyc.com/gbfs/en/station_information.json)')
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+//   'https://gbfs.citibikenyc.com/gbfs/en/station_information.json')
+
+let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href='https://gbfs.citibikenyc.com/gbfs/en/station_information.json</a>'
+})
+// d3.json('(https://gbfs.citibikenyc.com/gbfs/en/station_information.json)')
 
   // Create a baseMaps object to hold the lightmap layer.
-
+var baseMaps = {
+  "Street Map": street,
+  "Topographic Map": topo
+  };
 
   // Create an overlayMaps object to hold the bikeStations layer.
-
+var overlayMaps = {
+  "State Population": states,
+  "City Population": cities
+  };
 
   // Create the map object with options.
-
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5,
+  layers: [street, states, cities]
+  });
 
   // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+  }).addTo(myMap);
 
 // Create the createMarkers function.
 
